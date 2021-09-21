fdec7acf8201bb3ca39f1ff9f16c90c812854451
Meeeri08 <61042895+Meeeri08@users.noreply.github.com>
2021-09-21 18:14:01 +0200

    Imgui Files

--- Application.cpp
+++ Application.cpp
@@ -8,7 +8,6 @@ Application::Application()
 	scene_intro = new ModuleSceneIntro(this);
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
-	editor = new Editor(this);
 
 	// The order of calls is very important!
 	// Modules will Init() Start() and Update in this order
@@ -19,7 +18,6 @@ Application::Application()
 	AddModule(camera);
 	AddModule(input);
 	AddModule(audio);
-	AddModule(editor);
 	
 	// Scenes
 	AddModule(scene_intro);
--- Editor.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EDITOR_H__
-#define __EDITOR_H__
-
-#include "Module.h"
-#include "Globals.h"
-#include "imgui-master/imgui.h"
-
-
-#endif // __EDITOR_H__
